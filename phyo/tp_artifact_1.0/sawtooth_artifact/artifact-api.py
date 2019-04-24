59d991ebcc55ae92d155d92570f3df042d6bd4ba
Phyo Htut <pkthein@gmail.com>
2019-04-24 20:41:54 +0000

    small mod on artifact-api

--- phyo/tp_artifact_1.0/sawtooth_artifact/artifact-api.py
+++ phyo/tp_artifact_1.0/sawtooth_artifact/artifact-api.py
@@ -11,6 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------------
+################################################################################
+#                               LIBS & DEPS                                    #
+################################################################################
 from flask import Flask, jsonify, make_response, request, json
 import artifact_cli
 import configparser
